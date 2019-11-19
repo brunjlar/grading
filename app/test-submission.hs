58320e49658a4fba422a3ff5c386a93d4696343a
Lars Brünjes <brunjlar@gmail.com>
2019-11-19 14:44:20 +0100

    first version of executable

--- app/test-submission.hs
+++ app/test-submission.hs
@@ -2,5 +2,13 @@ module Main
     ( main
     ) where
 
+import System.Environment    (getArgs)
+import TestSubmission.Submit
+
 main :: IO ()
-main = putStrLn "test-submission"
+main = do
+    [n, s] <- getArgs
+    putStrLn $ "container name: " ++ n
+    putStrLn $ "submission: " ++ s
+    res <- submit n s
+    print res
--- src/TestSubmission/Submit.hs
+++ src/TestSubmission/Submit.hs
@@ -1,8 +1,30 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module TestSubmission.Submit
     ( submit
     ) where
 
+import Control.Exception (try, SomeException)
+import System.FilePath ((</>), (<.>))
+import TestSubmission.Docker
 import TestSubmission.Result
+import UnliftIO.Temporary (withSystemTempDirectory)
 
-submit :: FilePath -> IO Result
-submit submission = undefined
+submit :: ContainerName -> FilePath -> IO Result
+submit n submission = withSystemTempDirectory "temp" $ \fp ->
+    withDetachedContainer n (Just "/test/") $ \cid -> do
+        let extractLog = fp </> "extract" <.> "log"
+        let buildLog = fp </> "build" <.> "log"
+        let testLog = fp </> "test" <.> "log"
+        let hlintLog = fp </> "hlint" <.> "log"
+        copyToContainer cid submission "/test/solution.tar.gz"
+        e <- try $ do
+            execInContainer cid "./test-internal.sh"
+            copyFromContainer cid "/test/extract.log" extractLog
+            copyFromContainer cid "/test/build.log" buildLog
+            copyFromContainer cid "/test/test.log" testLog
+            copyFromContainer cid "/test/hlint.log" hlintLog
+        case e of
+            Left (ex :: SomeException) -> putStrLn $ "EXCEPTION: " ++ show ex
+            Right ()                   -> putStrLn "OK"
+        toResult extractLog buildLog testLog hlintLog 
